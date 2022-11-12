603cf24ca964b4b8f29c98bf4e46623559d99753
Ömer NAYIR <omer.nayir.mdbf17@iste.edu.tr>
2022-11-12 14:40:39 +0300

    components added and styled components updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import Navbar from './components/Navbar/Navbar';
+import Container from './components/Container';
+import Footer from './components/Footer';
 
 function App() {
+
+  const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  cursor: pointer;
+  transition: 0.4s;
+  &:hover {
+    border: 2px solid green;
+    width: 10%;
+    height: 10%;
+  }
+  `
+  const Div = styled.div`
+    top:0;
+    left:0;
+    bottom:0;
+    right:0;
+    margin:0;
+    padding:0;
+    width:100%;
+    height: 100vh;
+  `
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Div>
+      <Navbar />
+      <Container />
+      <Button>Click</Button>
+      <Footer />
+    </Div>
   );
 }
 
---
+++ src/components/Container.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Search from './Search'
+
+const Container = () => {
+    return (
+        <div>
+            Container
+            <Search />
+        </div>
+    )
+}
+
+export default Container
